6990575d827c5ecabca59af113c05401e84bcc58
DimaLukyanov <dima_lol_cool@bk.ru>
2023-05-15 11:03:47 +0300

    DML

---
+++ DML/INSERT_Table_3.sql
@@ -0,0 +1,8 @@
+-- Заполнение таблицы countries
+INSERT INTO countries (name)
+VALUES
+  ('Country 1'),
+  ('Country 2'),
+  ('Country 3'),
+  ('Country 4'),
+  ('Country 5');
---
+++ DML/INSERT_Table_4.sql
@@ -0,0 +1,8 @@
+-- Заполнение таблицы directors
+INSERT INTO directors (first_name, last_name)
+VALUES
+  ('Director 1', 'Lastname 1'),
+  ('Director 2', 'Lastname 2'),
+  ('Director 3', 'Lastname 3'),
+  ('Director 4', 'Lastname 4'),
+  ('Director 5', 'Lastname 5');
